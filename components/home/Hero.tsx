d440ca1778384b3ba719282df4c72527b7d9daa0
unknown <terkinasbox@gmail.com>
2023-04-17 17:37:35 +0300

    fixed coloring

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -5,13 +5,13 @@ type Props = {}
 
 export default function Hero({}: Props) {
   return (
-    <header className='w-full h-full'>
+    <header className='w-full h-full flex flex-col gap-12'>
 
       
 
-      <div className='w-11/12 mx-auto mt-12 h-[76vh]'>
+      <div className='w-11/12 mx-auto mt-12'>
         <div className='flex flex-col gap-2'>
-            <h1 className='text-5xl font-semibold leading-tight text-blue-800'>Hey there,<br /> I’m Arnold</h1>
+            <h1 className='text-5xl font-semibold leading-tight text-secondary'>Hey there,<br /> I’m Arnold</h1>
             <h2 className=''>I design beautifully simple things,<br /> and I love what I do.</h2>
         </div>
 
@@ -28,8 +28,8 @@ export default function Hero({}: Props) {
         </div>
       </div>
 
-      <div className='flex justify-start items-end'>
-        <Image className='absolute' src='/images/home/portrait.png' alt={'Portrait of Arnold'} width={500} height={500} />
+      <div className='flex justify-center items-end h-fit max-w-lg'>
+        <Image className='relative h-full' src='/images/home/portrait.png' alt={'Portrait of Arnold'} width={330} height={330} />
       </div>
     </header>
   )
